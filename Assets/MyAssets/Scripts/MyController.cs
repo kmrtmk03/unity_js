bf59c878e81b964077b20915ca7e817c367c360d
kimura <t.kimura@zizo.ne.jp>
2021-11-10 21:57:37 +0900

    add モデル追加したり

--- Assets/MyAssets/Scripts/MyController.cs
+++ Assets/MyAssets/Scripts/MyController.cs
@@ -19,29 +19,53 @@ namespace MyAssets
 
         private void Update()
         {
+            //前後
             if (Input.GetKey(KeyCode.W))
             {
-                gameObject.transform.position += transform.forward * Time.deltaTime * 2.0f;
-                animator.SetBool("isWalk", true);
+                this.WalkFront();
             }
             else if(Input.GetKey(KeyCode.S))
             {
-                gameObject.transform.position -= transform.forward * Time.deltaTime * 1.0f;
-                animator.SetBool("isWalk", true);
+                this.WalkBack();
             }
-            else if (Input.GetKey(KeyCode.D))
+
+            //左右
+            if (Input.GetKey(KeyCode.D))
             {
-                gameObject.transform.Rotate(Vector3.up * Time.deltaTime * 150.0f);
+                this.TurnRight();
             }
             else if (Input.GetKey(KeyCode.A))
             {
-                gameObject.transform.Rotate(Vector3.up * Time.deltaTime * -150.0f);
+                this.TurnLeft();
             }
 
             if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
             {
                 animator.SetBool("isWalk", false);
             }
+        }
+
+
+        public void WalkFront()
+        {
+            gameObject.transform.position += transform.forward * Time.deltaTime * 2.0f;
+            animator.SetBool("isWalk", true);
+        }
+
+        public void WalkBack()
+        {
+            gameObject.transform.position -= transform.forward * Time.deltaTime * 1.0f;
+            animator.SetBool("isWalk", true);
+        }
+
+        public void TurnLeft()
+        {
+            gameObject.transform.Rotate(Vector3.up * Time.deltaTime * -100.0f);
+        }
+
+        public void TurnRight()
+        {
+            gameObject.transform.Rotate(Vector3.up * Time.deltaTime * 100.0f);
         }
     }
 }
