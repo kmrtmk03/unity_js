2bcafc5843d14e17180c8c1a9bcac74bc467ca2b
kimura <t.kimura@zizo.ne.jp>
2022-01-11 12:22:34 +0900

    [update] collider周りを修正

--- Assets/MyAssets/Scripts/PhotoTrigger.cs
+++ Assets/MyAssets/Scripts/PhotoTrigger.cs
@@ -9,17 +9,30 @@ public class PhotoTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("photo-3");
+        GameObject gameObject = other.gameObject;
+        bool isPlayer = gameObject.GetComponent<MyCharacter>().isActiveAndEnabled;
+
+        if(isPlayer)
+        {
+            Debug.Log("Hit Collider");
 #if !UNITY_EDITOR
-        Calljs.JsDisplayModalInt(myIndex);
+            Calljs.JsDisplayModalInt(myIndex);
 #endif
+        }
     }
 
 
     private void OnTriggerExit(Collider other)
     {
+        GameObject gameObject = other.gameObject;
+        bool isPlayer = gameObject.GetComponent<MyCharacter>().isActiveAndEnabled;
+
+        if (isPlayer)
+        {
+            Debug.Log("Leave Collider");
 #if !UNITY_EDITOR
-        Calljs.JsHiddenModal();
+            Calljs.JsHiddenModal();
 #endif
+        }
     }
 }
